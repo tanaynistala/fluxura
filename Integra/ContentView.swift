999a3ca7f83efaea1b93fd9354328b0e5db5e2cf
Tanay Nistala <nistalatanay@gmail.com>
2020-06-15 10:21:30 +0530

    Initial Commit

---
+++ Integra/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Integra
+//
+//  Created by Tanay Nistala on 6/15/20.
+//  Copyright © 2020 Tanay Nistala. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
